89c26964f0c72b8d25935cc87f25346e45bde8ca
Artyom <artyomd2009@yandex.ru>
2019-02-23 19:59:37 +0300

    delete extra moduls

--- server/index.js
+++ server/index.js
@@ -4,18 +4,9 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 
-app.engine('.hbs', exphbs({
-    defaultLayout: 'main',
-    extname: '.hbs',
-    layoutsDir: path.join(__dirname, 'views/layouts')
-}))
-
-
-app.set('view engine', '.hbs')
-app.set('views', path.join(__dirname, '../views'))
-
 
 app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/../public')
 app.set('view engine', 'html')
 
 app.use(express.static('./static'))
