d85907eb00256788c0be5f7263af37164d5cc5b1
Bigyin <sergei.aparin1955@yandex.ru>
2019-03-24 02:35:45 +0300

    logout

--- public/main.js
+++ public/main.js
@@ -6,8 +6,10 @@ import {AuthorsView} from './views/AuthorsView/Authors.js';
 import {SignUpView} from './views/SignUpView/SignUp.js';
 import {SignInView} from './views/SignInView/SignIn.js';
 import {ProfileView} from './views/ProfileView/Profile.js';
+import signOut from './views/SignOut/SignOut';
 import {checkAuth} from './utils/user.js';
 import Router from './utils/router';
+import bus from './utils/bus.js';
 
 const root = document.getElementById('application');
 
@@ -22,6 +24,7 @@ router
     .register('/sign_in', SignInView)
     .register('/profile', ProfileView);
 
+bus.on('logout', signOut);
 checkAuth(router.start.bind(router));
 
 export default router;
--- public/views/SignOut/SignOut.js
+++ public/views/SignOut/SignOut.js
@@ -2,27 +2,20 @@ import {Net} from '../../utils/net.js';
 import {User} from '../../utils/user.js';
 import router from '../../main';
 import Bus from '../../utils/bus';
-/** */
-class SignOut {
-  /**
-   *
-   */
-  constructor() {
-    Bus.on('logout', this.signOut.bind(this));
-  }
-  /**
-   * Выход и возвращение в главное меню
-   * @param {User} User
-  */
-  signOut() {
-    Net.delete({url: '/session'})
-        .then((resp) => {
-          if (resp.status === 200) {
-            User.removeUser();
-            Bus.emit('userUpdate', null);
-            router.open('/');
-          }
-        });
-  }
-}
-export const signOut = new SignOut();
+
+
+/**
+ * Выход и возвращение в главное меню
+*/
+export default function signOut() {
+  Net.delete({url: '/session'})
+      .then((resp) => {
+        if (resp.status === 200) {
+          User.removeUser();
+          Bus.emit('userUpdate', null);
+          router.open('/');
+        }
+      });
+};
+console.log('logout on')
+;
