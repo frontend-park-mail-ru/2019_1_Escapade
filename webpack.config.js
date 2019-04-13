41339265b96bdca4f950470b2939b1398a2ebd99
Ivan Spasenov <spasenovivan@mail.ru>
2019-04-13 21:59:11 +0300

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,22 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                targets: {
+                  edge: 15,
+                },
+              }],
+            ],
+          },
+        },
+      },
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
