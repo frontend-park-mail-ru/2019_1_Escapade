e337bdb8d2708bb1bdddde26aba87ef977db3d61
Bigyin <sergei.aparin1955@yandex.ru>
2019-05-02 03:13:45 +0300

    main menu responsive

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,10 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
         test: /\.pug$/,
         loader: 'pug-loader',
       },
