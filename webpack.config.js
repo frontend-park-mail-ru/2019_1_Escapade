a3f2de311874b3c78bff60f38beea1e50501cdae
Bigyin <sergei.aparin1955@yandex.ru>
2019-05-24 05:03:53 +0300

    add postcss

--- webpack.config.js
+++ webpack.config.js
@@ -26,33 +26,32 @@ module.exports = {
   },
 
   module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        loader: 'ts-loader',
-      },
-      {
-        test: /\.js$/,
-        use: ['source-map-loader'],
-        enforce: 'pre',
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.pug$/,
-        loader: 'pug-loader',
-      },
-      {
-        test: /\.(img|jpeg|jpg|png)$/,
-        loader: 'file-loader?name=img/[name].[ext]',
-      },
+    rules: [{
+      test: /\.ts$/,
+      exclude: /node_modules/,
+      loader: 'ts-loader',
+    },
+    {
+      test: /\.js$/,
+      use: ['source-map-loader'],
+      enforce: 'pre',
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+    },
+    {
+      test: /\.pug$/,
+      loader: 'pug-loader',
+    },
+    {
+      test: /\.(img|jpeg|jpg|png)$/,
+      loader: 'file-loader?name=img/[name].[ext]',
+    },
     ],
   },
 };
